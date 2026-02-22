b146404a8f7c9c4cc4ce04d8067b6c372278ff16
Mahak831 <mahakrajput831@gmail.com>
2026-02-22 07:58:33 +0530

    new commit

--- app.js
+++ app.js
@@ -2,18 +2,13 @@ const express=require('express');
 const app=express();
 const morgan=require('morgan');
 app.use(morgan("combined"));
+app.set('view engine','ejs');
 
-
-app.get("/about",function(req,res,next){
-  res.send("heyy");
-})
-app.get("/profile/:username",function(req,res){
-  res.send(req.params.username +'s page');
+app.get("/",function(req,res,next){
+  res.render("index");
 })
 
-app.get("/about/:username/:age",function(req,res){
-  res.send(`something about ${req.params.username} and ${req.params.age}`);
-})
+
 
 
 
