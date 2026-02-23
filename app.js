6599803afd8b4967a30220430148a203785e67c1
Mahak831 <mahakrajput831@gmail.com>
2026-02-23 18:57:40 +0530

    nww

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express=require('express');
 const app=express();
 const morgan=require('morgan');
-app.use(morgan("combined"));
-app.set('view engine','ejs');
 
-app.get("/",function(req,res,next){
+app.set('view engine','ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.get("/", function(req,res){
   res.render("index");
+}); 
+app.get("/check",function(req,res){
+  console.log(req.query);
+  res.send("workingggg");
 })
 
 
