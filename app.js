e6695c2347705deaae13b6c027a838ade780d546
Mahak831 <mahakrajput831@gmail.com>
2026-03-01 10:24:30 +0530

    new commit

--- app.js
+++ app.js
@@ -1,23 +1,58 @@
 const express=require('express');
 const app=express();
-const morgan=require('morgan');
-
-app.set('view engine','ejs');
+const mongoose=require("mongoose");
+const mongooseconnection=require("./config/mongoose");
+const userModel=require("./models/user");
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-
-app.get("/", function(req,res){
-  res.render("index");
-}); 
-app.get("/check",function(req,res){
-  console.log(req.query);
-  res.send("workingggg");
+app.get("/",function(req,res){
+  res.send("Hello World");
 })
+// app.get("/update", async function(req,res){
+// let user= await userModel.findOneAndUpdate({name:"keshav"},{name:"ram"},{returnDocument: "after"});
+// res.send(user);
+
+// })
+
+// app.get("/delete",async function(req,res){
+//   let user=await userModel.findOneAndDelete({name:"ram"});
+//   console.log(user);
+// })
+// app.post("/create",async function(req,res,next){
+// let {name,username,email,password}=req.body;
+// let user= await userModel.create(
+//   {
+//     name,
+//     username,
+//     email,
+//     password
+//   }
+// )
+// res.send(user);
+
+// })
+
+// app.get("/users",async function(req,res,next){
+//   let users=await userModel.find();
+//   res.send(users);
+// })
+// app.get("/users/:username",async function(req,res,next){
+//   let user=await userModel.findOne({username:req.params.username});
+//   res.send(user);
+// })
+// app.get("/users/:username",async function(req,res,next){
+//   let {name,email,username}=req.body;
+//   let user=await userModel.findOneAndUpdate({username:req.params.username},{name,email,username},{returnDocument:"after"});
+//   res.send(user);
+//  })
 
 
-
-
-
+app.get("/users/:username",async function(req,res,next){
+  let {name,email,username}=req.body;
+  let user=await userModel.findOneAndDelete({username:req.params.username});
+  res.send(user);
+ })
 
 
 app.listen(3000);
+
---
+++ config/mongoose.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose');
+const debuglog=require("debug")("development:mongooseconfig");
+mongoose.connect("mongodb://127.0.0.1:27017/testingdb");
+const db=mongoose.connection;
+db.on("error",function(err){
+  console.log("Database error:", err);
+})
+db.on("open",function(err){
+  console.log("connected to database");
+
+})
+module.exports=db;
